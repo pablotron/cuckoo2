673035df9085c354226e5cffa326e1f5323067e7
pabs <pabs@pablotron.org>
2007-10-24 00:06:03 -0400

    populate hash code

--- include/cuckoo/hash.h
+++ include/cuckoo/hash.h
@@ -4,31 +4,18 @@
 #include <stdint.h>
 #include <unistd.h>
 
-typedef struct ck_hash_st ck_hash;
-
-typedef enum {
-  CK_OK,
-  CK_NONE,
-  CK_ERR_LAST
-} ck_err;
-
-ck_err ck_strerror(ck_err err, char *buf, size_t buf_len);
-
-typedef struct {
-  uint32_t keys[2];
-  void     *val;
-} ck_entry;
-
 typedef struct {
   uint32_t flags;
-  ck_err (*cb)(void *, size_t, uint32_t *, void *);
+
+  /* hash callback */
+  ck_err (*hash)(void *, size_t, uint32_t *, void *);
 
   ck_err (*malloc)(ck_hash *, size_t);
   ck_err (*realloc)(ck_hash *, size_t);
   ck_err (*free)(ck_hash *, void *);
 
   size_t growth_factor,
-         default_capa[2];
+         default_capa;
 } ck_hash_opt;
 
 struct ck_hash_st {
@@ -43,19 +30,18 @@ struct ck_hash_st {
   uint32_t  stats;
 };
 
-#define CK_HAHS(h) ((ck_hash*) (h))
+#define CK_HASH(h) ((ck_hash*) (h))
 #define CK_USED(h) ((h)->used[0] + (h)->used[1])
 #define CK_CAPA(h) ((h)->capa[0] + (h)->capa[1])
+#define CK_HAS(h, k, kl, kz) (ck_get((h), (k), (kl), (kz), NULL) != CK_NONE)
 
-ck_err ck_new(ck_hash_opt *, ck_hash *ret);
-ck_err ck_free(ck_hash *hash);
+ck_err ck_init(ck_hash *hash, ck_hash_opt *);
+ck_err ck_fini(ck_hash *hash);
 
 ck_err ck_key(ck_hash *hash, void *key, uint32_t key_len, uint32_t *ret);
 
 ck_err ck_get(ck_hash *hash, void *key, uint32_t key_len, uint32_t *keys, void **ret);
 ck_err ck_set(ck_hash *hash, void *key, uint32_t key_len, uint32_t *keys, void *val);
 ck_err ck_rm(ck_hash *hash, void *key, uint32_t key_len, uint32_t *keys, void **ret);
-
-#define ck_has(h, k, kl, kz) (ck_get((h), (k), (kl), (kz), NULL) != CK_NONE)
 
 #endif /* CK_HASH_H */
--- src/hash.c
+++ src/hash.c
@@ -1,61 +1,85 @@
-#ifndef CK_HASH_H
-#define CK_HASH_H
+#include "ck_private.h"
 
-#include <stdint.h>
-#include <unistd.h>
-
-typedef struct ck_hash_st ck_hash;
-
-typedef enum {
-  CK_OK,
-  CK_NONE,
-  CK_ERR_LAST
-} ck_err;
-
-ck_err ck_strerror(ck_err err, char *buf, size_t buf_len);
-
-typedef struct {
-  uint32_t keys[2];
-  void     *val;
-} ck_entry;
-
-typedef struct {
-  uint32_t flags;
-  ck_err (*cb)(void *, size_t, uint32_t *, void *);
-
-  ck_err (*malloc)(ck_hash *, size_t);
-  ck_err (*realloc)(ck_hash *, size_t);
-  ck_err (*free)(ck_hash *, void *);
-
-  size_t growth_factor,
-         default_capa[2];
-} ck_hash_opt;
-
-struct ck_hash_st {
-  uint32_t flags;
-  ck_hash_opt *opt;
-
-  /* entry tables */
-  ck_entry *bins[2];
-  size_t    used[2], 
-            capa[2];
-
-  uint32_t  stats;
-};
-
-#define CK_HAHS(h) ((ck_hash*) (h))
+#if 0
 #define CK_USED(h) ((h)->used[0] + (h)->used[1])
 #define CK_CAPA(h) ((h)->capa[0] + (h)->capa[1])
-
-ck_err ck_new(ck_hash_opt *, ck_hash *ret);
-ck_err ck_free(ck_hash *hash);
-
-ck_err ck_key(ck_hash *hash, void *key, uint32_t key_len, uint32_t *ret);
-
-ck_err ck_get(ck_hash *hash, void *key, uint32_t key_len, uint32_t *keys, void **ret);
-ck_err ck_set(ck_hash *hash, void *key, uint32_t key_len, uint32_t *keys, void *val);
-ck_err ck_rm(ck_hash *hash, void *key, uint32_t key_len, uint32_t *keys, void **ret);
-
 #define ck_has(h, k, kl, kz) (ck_get((h), (k), (kl), (kz), NULL) != CK_NONE)
+#endif /* 0 */
 
-#endif /* CK_HASH_H */
+static ck_err 
+ck_init_with_size(ck_hash *hash, ck_hash_opt *opt, size_t size) {
+  ck_err err;
+
+  /* check for return buffer */
+  if (!hash)
+    return CK_ERR_NULL_HASH;
+
+  /* set default options */
+  if (!opt)
+    opt = ck_get_default_opt();
+
+  /* clear and populate hash */
+  memset(hash, 0, sizeof(ck_hash));
+  hash->opt = opt;
+
+  /* calculate size and make sure it's a multiple of 3 */
+  size = (size > 0) ? size : opt->default_capa;
+  if (mod = (size % 3)) 
+    size += 3 - mod;
+
+  /* get size in bytes of bins */
+  size = size * sizeof(ck_entry);
+
+  /* allocate bins */
+  if ((hash->bins = opt->malloc(hash, size)) == NULL)
+    return CK_ERR_NOMEM;
+
+  /* populate bin capacities */
+  hash->capa[0] = 2 * size / 3;
+  hash->capa[1] = size / 3;
+
+  /* return success */
+  return CK_OK;
+}
+
+ck_err 
+ck_init(ck_hash *hash, ck_hash_opt *opt) {
+  return ck_init_with_size(hash, opt, 0);
+}
+
+ck_err 
+ck_fini(ck_hash *hash) {
+  ck_err err;
+
+  /* free bins */
+  if ((err = hash->opt->free(opt, hash->bins)) != CK_OK)
+    return err;
+
+  /* clear out internal elements */
+  memset(hash, 0, sizeof(ck_hash));
+
+  /* return success */
+  return CK_OK;
+}
+
+ck_err 
+ck_key(ck_hash *hash, void *key, uint32_t key_len, uint32_t *ret) {
+  if (!key || !ret)
+    return CK_ERR_NULL_BUF;
+  if (!hash)
+    return CK_ERR_NULL_HASH;
+
+  /* hash value */
+  return hash->opt->hash(hash, key, key_len, ret);
+}
+
+ck_err 
+ck_get(ck_hash *hash, void *key, uint32_t key_len, uint32_t *keys, void **ret) {
+}
+
+ck_err 
+ck_set(ck_hash *hash, void *key, uint32_t key_len, uint32_t *keys, void *val) {
+}
+
+ck_err ck_rm(ck_hash *hash, void *key, uint32_t key_len, uint32_t *keys, void **ret) {
+}
