290863b080fa613de5945e4a34b8e188584c34de
pabs <pabs@pablotron.org>
2007-10-23 23:36:54 -0400

    added error, structure, etc

---
+++ cuckoo.h
@@ -0,0 +1,19 @@
+
+#ifndef CUCKOO_H
+#define CUCKOO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include <stdint.h>
+#include <unistd.h>
+
+#include <cuckoo/error.h>
+#include <cuckoo/hash.h>
+
+#ifdef __cplusplus
+};
+#endif /* __cplusplus */
+
+#endif /* CUCKOO_H */
